6813f78a13af120739b7de0f96d3c3a0701502a6
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-02 12:01:21 +0400

    Last updated

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const middleHr = document.getElementById('middle-hr');
 const bottomHr = document.getElementById('bottom-hr');
 const dragButton = document.getElementById('drag-button');
 const fourDots = document.getElementById('four-dots');
+const tasksList = document.getElementById('buttons-and-input-field');
 const input = document.getElementById('input');
 const deleteButton = document.getElementById('delete-button');
 const addButton = document.getElementById('add-button');
 
+addButton.addEventListener('click', addNewTask);
+
+function addNewTask() {
+    
+}
