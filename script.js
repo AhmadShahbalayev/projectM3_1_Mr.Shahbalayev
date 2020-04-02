95c67e1641316fe89786c35ac09633812b3e528c
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-02 11:38:08 +0400

    Last updated

---
+++ script.js
@@ -0,0 +1,11 @@
+const sortButton = document.getElementById('sort-button');
+const arrow = document.getElementById('arrow');
+const topHr = document.getElementById('top-hr');
+const middleHr = document.getElementById('middle-hr');
+const bottomHr = document.getElementById('bottom-hr');
+const dragButton = document.getElementById('drag-button');
+const fourDots = document.getElementById('four-dots');
+const input = document.getElementById('input');
+const deleteButton = document.getElementById('delete-button');
+const addButton = document.getElementById('add-button');
+
