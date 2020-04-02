318d2df7338fe5895bcc055e0e81189ee413f981
AhmadShahbalayev <ahmedsahbalayev@gmail.com>
2020-04-02 17:30:44 +0400

    Last updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const mainBody = document.getElementById('main-body');
 const sortButton = document.getElementById('sort-button');
 const arrow = document.getElementById('arrow');
 const topHr = document.getElementById('top-hr');
 const middleHr = document.getElementById('middle-hr');
 const bottomHr = document.getElementById('bottom-hr');
-const input = document.querySelector('.input');
+const mainBody = document.getElementById('task-list-div');
+const input = document.getElementById('input');
 const deleteButton = document.querySelector('.delete-button');
 const addButton = document.getElementById('add-button');
 
@@ -14,11 +14,10 @@ function addNewTask() {
     // CREATES NEW BUTTONS AND INPUT FIELD:
     const newButtonsAndInputField = document.createElement('div');
     newButtonsAndInputField.classList.add('buttons-and-input-field');
-    mainBody.insertBefore(newButtonsAndInputField, mainBody.childNodes[4]);
     // CREATES NEW DRAG BUTTON FIELD:
     const newDragButtonField = document.createElement('div');
     newDragButtonField.classList.add('drag-button-field');
-    mainBody.insertBefore(newDragButtonField, mainBody.childNodes[5]);
+    newButtonsAndInputField.append(newDragButtonField);
     // CREATES NEW DRAG BUTTON:
     const newDragButton = document.createElement('button');
     newDragButton.classList.add('drag-button');
@@ -27,8 +26,10 @@ function addNewTask() {
     // CREATES NEW INPUT FIELD:
     const newInputField = document.createElement('div');
     newInputField.classList.add('input-field');
-    newInputField.innerHTML = '<input type="text" name="_input" class="input">';
-    mainBody.insertBefore(newInputField, mainBody.childNodes[6]);
+    newInput = document.createElement('input');
+    newInput.classList.add('input');
+    newInputField.appendChild(newInput);
+    newButtonsAndInputField.append(newInputField);
     // CREATES NEW DELETE BUTTON FIELD:mainBody
     const newDeleteButtonField = document.createElement('div');
     // CREATES NEW DELETE BUTTON:
@@ -36,5 +37,11 @@ function addNewTask() {
     newDeleteButton.classList.add('delete-button');
     newDeleteButton.innerHTML = '<span class="delete-mark">&#10005;</span>';
     newDeleteButtonField.appendChild(newDeleteButton);
-    mainBody.insertBefore(newDeleteButtonField, mainBody.childNodes[7]);
+    newButtonsAndInputField.append(newDeleteButtonField);
+    appendTask(newButtonsAndInputField);
 }
+
+function appendTask(task) {
+    mainBody.append(task);
+}
+
